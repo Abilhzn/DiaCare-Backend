e60875d984cc28f6de16c3f924163eff3e398e4d
azzahrasabryna <azzahrasabryna@gmail.com>
2025-06-11 17:21:36 +0700

    configggggg

---
+++ app/auth/config.py
@@ -0,0 +1,52 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    """
+    Konfigurasi dasar yang akan diwarisi oleh konfigurasi lainnya.
+    """
+    # Kunci rahasia untuk keamanan sesi dan CSRF.
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci-rahasia-yang-sangat-sulit-ditebak'
+
+    # Menonaktifkan fitur modifikasi dari SQLAlchemy yang tidak diperlukan dan deprecated.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Kunci rahasia untuk JSON Web Tokens (JWT).
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'kunci-jwt-super-rahasia'
+
+    MODEL_PATH = os.path.join(basedir, 'app', 'diabetes_prediction', 'ml_model', 'diabetes_model.pkl')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    # Mengaktifkan mode debug Flask untuk hot-reloading dan debugger interaktif.
+    DEBUG = True
+
+    # URI untuk database pengembangan.
+    # Menggunakan SQLite agar mudah untuk memulai.
+    # File database (diacare_dev.db) akan dibuat di direktori root proyek.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'diacare_dev.db')
+
+class TestingConfig(Config):
+    # Mengaktifkan mode testing Flask.
+    TESTING = True
+
+    # Menonaktifkan CSRF protection dalam form saat testing.
+    WTF_CSRF_ENABLED = False
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///:memory:'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'diacare.db')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
