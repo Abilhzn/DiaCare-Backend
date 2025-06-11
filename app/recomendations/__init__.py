afb305c0eb28af48488f1261f0da4a33f160954e
aazaleaf <sitinurkhomariah64@gmail.com>
2025-06-12 00:39:02 +0700

    benerin init, ver 3.5

--- app/recomendations/__init__.py
+++ app/recomendations/__init__.py
@@ -1,10 +1,4 @@
-from flask import Flask
+from flask import Blueprint
 
-def create_app():
-    app = Flask(__name__)
-
-    # --- Register Blueprints ---
-    from app.recommendations.routes import recommendations_bp
-    app.register_blueprint(recommendations_bp, url_prefix='/api/recommendations')
-
-    return app
+recommendations_bp = Blueprint('recommendations', __name__)
+from . import routes
